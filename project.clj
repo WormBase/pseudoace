134b200f962016c9bef8263a8b87800894a8ca89
Adam Wright <adam.wright@wormbase.org>
2016-02-26 20:46:21 +0000

    removing clojure script resources and changing project.clj to work with dynamodb

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject pseudoace "0.0.3-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.datomic/datomic-free "0.9.5186" :exclusions [joda-time]]
+ ;;                [com.datomic/datomic-free "0.9.5186" :exclusions [joda-time]]
+                 [com.datomic/datomic-pro "0.9.5344" :exclusions [joda-time org.slf4j/slf4j-nop]] ;; added
+                 [com.amazonaws/aws-java-sdk-dynamodb "1.9.39" :exclusions [joda-time]] ;; added
                  [datomic-schema "1.3.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [acetyl "0.0.9-SNAPSHOT"]
@@ -33,29 +35,18 @@
 
                  [org.clojure/tools.cli "0.3.3"]
                  [org.clojars.hozumi/clj-commons-exec "1.0.6" ]]
-  :description "ACeDB emulator and migration tools"
+  :description "ACeD migration tools"
   :source-paths ["src/clj/pseudoace", "src/clj/", "src/perl/"]
   :java-source-paths ["src/java"]
-  :resource-paths ["resources"]
- 
  
   :main ace-to-datomic
 
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-environ "1.0.0"]]
+  :plugins [[lein-environ "1.0.0"]]
 
-  :cljsbuild {
-    :builds [{:id "dev"
-              :source-paths ["src/cljs"]
-              :compiler {
-                :optimizations :whitespace
-                :output-to "resources/public/js/main.js"
-                :output-dir "resources/public/js/out"
-                :source-map "resources/public/js/main.js.map"}}]}
-  
   :javac-options ["-target" "1.6" "-source" "1.6"]
   :license "MIT"
   :repositories [["dasmoth" {:url "http://www.biodalliance.org/people/thomas/repo"}]]
+ 
   :jvm-opts ["-Xmx6G"
              "-XX:+UseG1GC" "-XX:MaxGCPauseMillis=50"  ;; same GC options as the transactor,
                                                        ;; should minimize long pauses.
