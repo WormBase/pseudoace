2f5eaf5c8401fd557bc2193fd937407ac0bdcf0a
Matt Russell <matt@mgrbyte.co.uk>
2016-04-01 16:57:19 +0100

    whitespace.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject wormbase/pseudoace "0.0.3-SNAPSHOT"
   :dependencies [;; TODO: this should pull from wormbase/acetyl when available.
-                 [acetyl "0.0.9-SNAPSHOT"] 
+                 [acetyl "0.0.9-SNAPSHOT"]
                  [com.amazonaws/aws-java-sdk-dynamodb "1.9.39"
                   :exclusions [joda-time]]
                  [com.datomic/datomic-pro "0.9.5350"
@@ -16,9 +16,9 @@
   :repositories [["dasmoth" {:url "http://www.biodalliance.org/people/thomas/repo"}]]
   :jvm-opts ["-Xmx6G"
              ;; same GC options as the transactor,
-             "-XX:+UseG1GC" "-XX:MaxGCPauseMillis=50"  
+             "-XX:+UseG1GC" "-XX:MaxGCPauseMillis=50"
              ;; should minimize long pauses.
-             "-Ddatomic.objectCacheMax=2500000000" 
+             "-Ddatomic.objectCacheMax=2500000000"
              "-Ddatomic.txTimeoutMsec=1000000"
              ;; Uncomment to prevent missing trace (HotSpot optimisation)
              ;; "-XX:-OmitStackTraceInFastThrow"
