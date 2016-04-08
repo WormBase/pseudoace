ca5cac02f00c331d64014acd51ec786df7e94d00
Matt Russell <matthew.russell@wormbase.org>
2016-04-08 13:13:11 +0100

    Fix tests.

--- test/pseudoace/test_core.clj
+++ test/pseudoace/test_core.clj
@@ -1,27 +1,27 @@
-(ns pseudoace.test-ace-to-datomic
+(ns pseudoace.test-core
   (:require
    [clojure.java.io :as io]
    [clojure.test :refer (deftest is)]
-   [pseudoace.ace-to-datomic :as a2d]
+   [pseudoace.core :as core]
    [pseudoace.utils :as utils]))
 
 
 (deftest test-get-current-directory
-  (is (.exists (io/file (a2d/get-current-directory)))))
+  (is (.exists (io/file (core/get-current-directory)))))
 
 (deftest test-move-helper-log-file
   (when-let [dummy-log-dir (io/file
                             (System/getProperty "java.io.tmpdir")
                             "dummy-edn-log-dir")]
     (.mkdir dummy-log-dir)
-    (let [dummy-helper-file (io/file dummy-log-dir a2d/helper-filename)]
+    (let [dummy-helper-file (io/file dummy-log-dir core/helper-filename)]
       (utils/with-outfile dummy-helper-file
         (println "dummy help edn content"))
       ;; Call function under test
-      (a2d/move-helper-log-file (.getParent dummy-helper-file))
+      (core/move-helper-log-file (.getParent dummy-helper-file))
       ;; Check expectations
       (let [dummy-log-dir (.getParent dummy-helper-file)
-            expected-dest-file (a2d/helper-dest-file dummy-log-dir)]
+            expected-dest-file (core/helper-dest-file dummy-log-dir)]
         (is (.exists expected-dest-file))
         (is (not (.exists dummy-helper-file)))
         (io/delete-file expected-dest-file)
