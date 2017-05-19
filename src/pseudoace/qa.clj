c8b2d9779826a27da1cefeb0e663fa4ac0dd15d2
Matt Russell <matthew.russell@wormbase.org>
2017-05-19 09:56:06 +0100

    Fixed parsing of ACeDB reference id catalog.

--- src/pseudoace/qa.clj
+++ src/pseudoace/qa.clj
@@ -10,15 +10,22 @@
    [pseudoace.model2schema :refer [datomize-name]]
    [pseudoace.utils :refer [merge-pairs]]))
 
-(def ^:private report-headings
-  ["ACeDB-class" "datomic-ident" "Missing" "Added" "Identical"])
+(def ^{:private true} report-headings
+  ["ACeDB" "datomic" "Missing" "Added" "Identical"])
 
-(defn- write-append [writer record & {:keys [verbose]
-                                      :or {verbose false}}]
+(defn- write-append [writer record
+                     & {:keys [verbose]
+                        :or {verbose false}}]
   (let [s (csv/write-csv [record])]
     (when verbose
       (print s))
     (.write writer s)))
+
+(defn- merge-split-record [rec]
+  (list (first rec)
+        (->> (rest rec)
+             (str/join ",")
+             (str/trim))))
 
 (defn read-ref-data
   "Read class data generated from a WormBase ACeDB database via `reader`.
@@ -26,12 +33,12 @@
   The file contains one line per class-value pair, in the format:
      className : identifier
 
-  Returns a mapping of className to set of identifiers per map.
-  "
+  Returns a mapping of className to set of identifiers per ACeDB class."
   [acedb-report-path]
   (with-open [rdr (io/reader acedb-report-path)]
-    (if-let [records (seq (csv/parse-csv rdr))]
-      (merge-pairs records))))
+    (->> (csv/parse-csv rdr)
+         (map merge-split-record)
+         (merge-pairs))))
 
 (defrecord StatsReport [class-names entries])
 
@@ -51,41 +58,42 @@
 
 (defn- report-entry [db ref-data native->ref attr]
   (let [class-name (native->ref attr)
-        query-result (d/q '[:find ?a ?v
-                            :in $ ?a
-                            :where [_ ?a ?v]]
-                          db attr)
-        mapped (or (merge-pairs query-result) {})
-        db-values (set (mapped attr))
+        db-values (->> (d/q '[:find [?v ...]
+                              :in $ ?a
+                              :where [_ ?a ?v]]
+                            db attr)
+                       (map pr-str)
+                       set)
         ref-values (set (ref-data class-name))
         [ref-only db-only in-both] (diff ref-values db-values)]
     (->StatsReportEntry class-name attr db-only ref-only in-both)))
 
 (defn- stats-data [report]
-  (let [entries (sort-by (comp namespace :attr) (:entries report))]
+  (let [attr-ns (comp namespace :attr)
+        entries (sort-by attr-ns (:entries report))]
     (for [entry entries]
       (if entry
-        (let [attr (:attr entry)
-              class-name (:class-name entry)
+        (let [class-name (:class-name entry)
+              attr-name (attr-ns entry)
               n-ref-only (.n-ref-only entry)
               n-db-only (.n-db-only entry)
               n-both (.n-both entry)]
-          (map str [class-name attr n-ref-only n-db-only n-both]))))))
+          (map str [class-name attr-name n-ref-only n-db-only n-both]))))))
 
 (defn report-import-stats
   "Returns a seqeunence of mappings of the diff
-   between `db` and `ref-data-path`."
+   between `:db/ident` values in datomic,
+   and ACeDB class IDs found in the file pointed to by `ref-data-path`."
   [db ref-data-path out-path & {:keys [verbose]
                                 :or {verbose false}}]
-  (let [class-names (->> (get-classes db)
-                         (map (comp :pace/identifies-class #(second %)))
-                         set
-                         sort)
-        native-names (map datomize-name class-names)
-        attrs (map #(keyword % "id") native-names)
-        native->ref (zipmap attrs class-names)]
-    (let [ref-data (read-ref-data ref-data-path)
-          rep-entry (partial report-entry db ref-data native->ref)
+  (let [ref-data (read-ref-data ref-data-path)
+        native->ref (into {} (d/q '[:find ?dn ?an
+                                    :where
+                                    [?e :db/ident ?dn]
+                                    [?e :pace/identifies-class ?an]] db))
+        attrs (keys native->ref)
+        class-names (vals native->ref)]
+    (let [rep-entry (partial report-entry db ref-data native->ref)
           report (->StatsReport class-names (pmap rep-entry attrs))]
       (spit out-path "" :append false)
       (with-open [wtr (io/writer out-path :append true)]
