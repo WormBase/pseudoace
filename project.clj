af424e6eb7aa1c2e13ff54c91d8b116d7dd4c8d3
Matt Russell <matthew.russell@wormbase.org>
2018-10-24 10:09:44 +0100

    Bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wormbase/pseudoace "0.5.7"
+(defproject wormbase/pseudoace "0.5.8"
   :dependencies [[clj-time "0.14.2"]
                  [clj-yaml "0.4.0"]
                  [clojure-csv/clojure-csv "2.0.2"]
