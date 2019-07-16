d80b1f723036abb7fa21d919270f21c8c9ff5902
Matt Russell <matthew.russell@wormbase.org>
2019-07-16 17:02:37 +0100

    Added cogntitect.transcriptor based tests for aceparsing.

---
+++ test/pseudoace/test_transcripts.clj
@@ -0,0 +1,12 @@
+(ns pseudoace.test-transcripts
+  (:require
+   [clojure.test :as t]
+   [cognitect.transcriptor :as xr]))
+
+(t/deftest test-all-transcripts
+  (prn (xr/repl-files "test/pseudoace/transcriptions"))
+  (doseq [repl-file (xr/repl-files "test/pseudoace/transcriptions")]
+    (xr/run repl-file)))
+
+
+
