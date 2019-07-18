57a7c2760331a89c3cb96400b9230572df827d71
Matt Russell <matthew.russell@wormbase.org>
2019-07-18 14:51:01 +0100

    Try deploying with simpler mvn command.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,10 @@
 
 LATEST_TAG=$(git describe --abbrev=0)
 RELEASE_NAME="pseudoace-${LATEST_TAG}"
-DEPLOY_JAR="${RELEASE_NAME}.jar"
+DEPLOY_JAR="target/${RELEASE_NAME}.jar"
 mkdir -p target
 clj -Spom
-clj -A:1.9:depstar -m hf.depstar.jar "target/${DEPLOY_JAR}"
-mvn deploy:deploy-file \
-     -Dfile="target/${DEPLOY_JAR}" \
-     -Drepository=clojars \
-     -Durl=https://clojars.org/repo \
-     -DpomFile=pom.xml
+clj -A:1.9:depstar -m hf.depstar.jar "${DEPLOY_JAR}"
+mvn deploy
 rm -rf target
 exit $?
