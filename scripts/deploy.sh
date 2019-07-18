c6215e816b9a535a4dad3a954ea003903ec536c5
Matt Russell <matthew.russell@wormbase.org>
2019-07-18 15:13:36 +0100

    Use vanilla deploy.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,10 @@
 LATEST_TAG=$(git describe --abbrev=0)
 RELEASE_NAME="pseudoace-${LATEST_TAG}"
 DEPLOY_JAR="target/${RELEASE_NAME}.jar"
+
+rm -rf target
 mkdir -p target
 clj -Spom
 clj -A:1.9:depstar -m hf.depstar.jar "${DEPLOY_JAR}"
-mvn deploy:deploy-file \
-     -Dfile="${DEPLOY_JAR}" \
-     -Drepository=clojars \
-     -Durl=https://clojars.org/repo \
-     -DpomFile=pom.xml
-rm -rf target
+mvn deploy
 exit $?
