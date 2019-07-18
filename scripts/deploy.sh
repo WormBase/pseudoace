3262b29675ddb0da3caba13f9153112aaf253330
Matt Russell <matthew.russell@wormbase.org>
2019-07-18 14:30:15 +0100

    Make clojars release use depstar "jar" (create library not uberjar)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,20 +2,14 @@
 
 LATEST_TAG=$(git describe --abbrev=0)
 RELEASE_NAME="pseudoace-${LATEST_TAG}"
-RELEASE_ARCHIVE="release-archives/${RELEASE_NAME}.tar.xz"
 DEPLOY_JAR="${RELEASE_NAME}.jar"
-
-if ! test -f "${RELEASE_ARCHIVE}"; then
-   echo "ERROR: jar file not built; run 'make bundle-release' first."
-   exit 1
-fi
-
-tar xf "${RELEASE_ARCHIVE}" "${RELEASE_NAME}/$DEPLOY_JAR"
+mkdir -p target
 clj -Spom
+clj -A:1.9:depstar -m hf.depstar.jar "target/${DEPLOY_JAR}"
 mvn deploy:deploy-file \
-     -Dfile="${DEPLOY_JAR}" \
+     -Dfile="target/${DEPLOY_JAR}" \
      -Drepository=clojars \
      -Durl=https://clojars.org/repo \
      -DpomFile=pom.xml
-rm -rf "${RELEASE_NAME}"
+rm -rf target
 exit $?
